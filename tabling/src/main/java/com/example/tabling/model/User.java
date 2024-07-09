b016ccb70837c8e4040aae6f9a407da484b32d06
이혜준 <lhg0004@douzone.com>
2024-07-09 22:14:53 +0900

    model > user

--- tabling/src/main/java/com/example/tabling/model/User.java
+++ tabling/src/main/java/com/example/tabling/model/User.java
@@ -1,5 +1,28 @@
 package com.example.tabling.model;
+import java.persistence.*;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
 
+    @Column
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String number;
+
+    private LocalDateTime createDate;
 }
